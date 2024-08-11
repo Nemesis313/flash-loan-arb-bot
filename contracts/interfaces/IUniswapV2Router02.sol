955b09739237e9b12829922d62d21bb737f48d09
nemesis <84901459+Nemesis313@users.noreply.github.com>
2024-08-11 11:59:12 +0330

    Update and rename Balancer-V2-Interface.js to IUniswapV2Router02.sol

---
+++ contracts/interfaces/IUniswapV2Router02.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.10;
+
+interface IUniswapV2Router02 {
+    function swapExactTokensForTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external;
+}
